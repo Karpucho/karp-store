5d931c4ea5744f866e9bc3e3f975319c4f718d7b
Karpucho <karpov.zgr@mail.ru>
2023-08-23 12:25:41 +0300

    feat: Добавил заготовки под роуты.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const sequelize = require('./db')
+const models = require('./models/models')
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Работаем' })
+// })
 
 const start = async () => {
   try {
